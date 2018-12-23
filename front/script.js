5ab596b531c06c2442264081814b6146854be4f7
Oseias Beu <oseiasbeu@outlook.com>
2018-12-23 16:28:58 -0200

    Criar métodos GET e POST

--- front/script.js
+++ front/script.js
@@ -1,3 +1,20 @@
-console.log("Funciona!!!")
+const axios = require("axios");
+const url = "http://127.0.0.1:8080/api/cliente";
 
-const axios = require('axios');
+let usuario  = {
+	"nome":"Bruce",
+	"nascimento":"1998-03-26 00:00:00",
+	"rg":"36.542.132-7",
+	"email":"bruce@wayne.com",
+	"endereco_id":2 
+}
+
+axios.get(url).then(function(resposta){
+  console.log(resposta.data.objects);
+}).catch(function(error){
+  if(error){
+    console.log(error);
+  }
+});
+
+axios.post(url, usuario)
